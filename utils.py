d3a25d98da273c77a26d46c9c193b78a1ef79a5d
molano <you@example.com>
2021-07-28 18:21:02 +0200

    load behavior and electro

--- utils.py
+++ utils.py
@@ -61,12 +61,7 @@ def get_behavior(main_folder, subject):
     p = ComPipe.chom(subject,  # sujeto (nombre de la carpeta under parentpath)
                      parentpath=main_folder,
                      analyze_trajectories=False)  # precarga sesiones disponibles
-    p.load_available()  # just in case, refresh
-    print(p.available[2])  # exmpl target sess / filename string is the actual arg
-    p.load(p.available[2])
-    p.process()
-    p.trial_sess.head()  # preprocessed df stored in attr. trial_sess
-    return p.sess
+    return p
 
 def get_startSound_times(df):    
     # STIM INITITAL PC-TIMES
@@ -312,3 +307,4 @@ def get_spikes(path):
 #     # plot_events(ttl_outc_strt, label='ttl-outcome', color='m')
 #     # plot_events(csv_so_sec, label='outcome', color='c', lnstl='--')
 #     # f.savefig('/home/molano/Dropbox/outcome_check.png')
+

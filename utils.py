a9486f4924ba3ea038441fd7f35d50036b9fbcb2
manuelmolano <manuelmolanomazon@gmail.com>
2021-07-30 17:29:19 +0200

    minor

--- utils.py
+++ utils.py
@@ -116,6 +116,7 @@ def get_electro(path, s_rate=3e4, s_rate_eff=2e3):
     data_files = glob.glob(path+'/*.dat')
     data_files = [f for f in data_files if 'temp' not in f]
     # assert len(data_files) == 1, 'Number of .dat files is '+str(len(data_files))
+    print('Number of .dat files is '+str(len(data_files)))
     print('Loading '+data_files[0])
     data = np.memmap(data_files[0], dtype='int16', mode='r')
     if len(data) % 40 == 0:

5b2a7f2d493980c12f9192710231593142b3ac9d
manuelmolano <manuelmolanomazon@gmail.com>
2021-07-30 18:11:31 +0200

    save inventory in dict

--- inventory.py
+++ inventory.py
@@ -20,17 +20,19 @@ def order_by_sufix(file_list):
 
 
 def inventory(s_rate=3e4, s_rate_eff=2e3):
-    spks_sorting_folder = '/archive/lbektic/AfterClustering/'
+    spks_sort_folder = '/archive/lbektic/AfterClustering/'
     electro_folder = '/archive/rat/electrophysiology_recordings/'
     behav_folder = '/archive/rat/behavioral_data/'
-    rats = glob.glob(spks_sorting_folder+'LE*')
+    rats = glob.glob(spks_sort_folder+'LE*')
+    inventory = {}
     for r in rats:
+        inventory[r] = {'ok': [], 'no_behavior': [], 'no_electro': [],
+                        'diff_num_events': [], 'too_much_diff': []}
         print('---------------')
         rat_num = r[r.find('/LE')+3:]
         print('Rat LE'+rat_num)
-        e_fs = glob.glob(electro_folder+'*'+str(rat_num)+'/*'+str(rat_num)+'*')
-        e_fs_bis = glob.glob(spks_sorting_folder+'*'+str(rat_num)+'/*'+
-                             str(rat_num)+'*')
+        e_fs = glob.glob(spks_sort_folder+'*'+str(rat_num)+'/*'+str(rat_num)+'*')
+        e_fs_bis = glob.glob(electro_folder+'*'+str(rat_num)+'/*'+str(rat_num)+'*')
         print('Number of electro sessions:', str(len(e_fs)))
         b_f = glob.glob(behav_folder+'*'+str(rat_num))
         assert len(b_f) == 1
@@ -47,6 +49,7 @@ def inventory(s_rate=3e4, s_rate_eff=2e3):
             if len(b_f) == 0:
                 print('---')
                 print(date+' behavioral file not found')
+                inventory[r]['no_behavior'].append(date)
                 continue
             elif len(b_f) > 1:
                 print('---')
@@ -73,6 +76,7 @@ def inventory(s_rate=3e4, s_rate_eff=2e3):
                     samples = utils.get_electro(path=e_f_bis[0], s_rate=s_rate,
                                                 s_rate_eff=s_rate_eff)
                 else:
+                    inventory[r]['no_electro'].append(date)
                     continue
             # get stim ttl starts/ends
             ttl_stim_strt, ttl_stim_end, _ =\
@@ -83,11 +87,17 @@ def inventory(s_rate=3e4, s_rate_eff=2e3):
                 print('Different number of start sounds')
                 print('CSV times', len(bhv_strt_stim_sec))
                 print('TTL times', len(ttl_stim_strt))
+                inventory[r]['diff_num_events'].append(date)
             else:
                 print('Median difference between start sounds')
                 print(np.median(bhv_strt_stim_sec-ttl_stim_strt))
                 print('Max difference between start sounds')
                 print(np.max(bhv_strt_stim_sec-ttl_stim_strt))
+                if np.max(bhv_strt_stim_sec-ttl_stim_strt) > 0.1:
+                    inventory[r]['too_much_diff'].append(date)
+                else:
+                    inventory[r]['ok'].append(date)
+    np.savez(spks_sort_folder+'/inventory.npz', **inventory)
 
 
 if __name__ == '__main__':

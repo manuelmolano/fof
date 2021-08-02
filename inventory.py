82133f120267277b17e56ab39c5e67bb9283798d
manuelmolano <manuelmolanomazon@gmail.com>
2021-08-02 15:44:51 +0200

    load only non-checked sessions

--- inventory.py
+++ inventory.py
@@ -40,15 +40,20 @@ def check_stim_starts(samples, chnls, s_rate, events, evnts_compare, inventory,
             inventory['ok'].append(date)
 
 
-def inventory(s_rate=3e4, s_rate_eff=2e3):
+def checked(dic, date):
+    return np.array([(np.array(v) == date).any() for v in dic.values()]).any()
+
+
+def inventory(s_rate=3e4, s_rate_eff=2e3):        
     spks_sort_folder = '/archive/lbektic/AfterClustering/'
     electro_folder = '/archive/rat/electrophysiology_recordings/'
     behav_folder = '/archive/rat/behavioral_data/'
     rats = glob.glob(spks_sort_folder+'LE*')
-    inventory = {}
+    inventory = np.load('/home/molano/fof/inventory.npz')
     for r in rats:
-        inventory[r] = {'ok': [], 'no_behavior': [], 'no_electro': [],
-                        'diff_num_events': [], 'too_much_diff': []}
+        if r not in inventory.keys():
+            inventory[r] = {'ok': [], 'no_behavior': [], 'no_electro': [],
+                            'diff_num_events': [], 'too_much_diff': []}
         print('---------------')
         rat_num = r[r.find('/LE')+3:]
         print('Rat LE'+rat_num)
@@ -66,45 +71,48 @@ def inventory(s_rate=3e4, s_rate_eff=2e3):
             date = e_f[dt_indx:dt_indx+10]
             e_f_bis = [f for f in e_fs_bis if f.find(date) != -1]
             date = date.replace('-', '')
-            b_f = [f for f in p.available if f.find(date) != -1]
-            if len(b_f) == 0:
-                print('---')
-                print(date+' behavioral file not found')
-                inventory[r]['no_behavior'].append(date)
-                continue
-            elif len(b_f) > 1:
-                print('---')
-                print(date+': several behavioral files found')
-                print(b_f)
-                sorted_files = order_by_sufix(file_list=b_f)
-                print('Files:')
-                print(sorted_files)
-                print('Used file: ', sorted_files[-1])
-                b_f = [sorted_files[-1]]
-            p.load(b_f[0])
-            p.process()
-            p.trial_sess.head()  # preprocessed df stored in attr. trial_sess
-            df = p.sess
-            bhv_strt_stim_sec, _ = utils.get_startSound_times(df=df)
-            bhv_strt_stim_sec -= bhv_strt_stim_sec[0]
-            try:
-                samples = utils.get_electro(path=e_f, s_rate=s_rate,
-                                            s_rate_eff=s_rate_eff)
-            except IndexError:
-                print('Electro file .dat not found in '+e_f)
-                if len(e_f_bis):
-                    print('Trying folder '+e_f_bis[0])
-                    samples = utils.get_electro(path=e_f_bis[0], s_rate=s_rate,
-                                                s_rate_eff=s_rate_eff)
-                else:
-                    inventory[r]['no_electro'].append(date)
+            if not checked(dic=inventory[r], date=date):
+                b_f = [f for f in p.available if f.find(date) != -1]
+                if len(b_f) == 0:
+                    print('---')
+                    print(date+' behavioral file not found')
+                    inventory[r]['no_behavior'].append(date)
                     continue
-            # get stim ttl starts/ends
-            check_stim_starts(samples=samples, chnls=[35, 36], s_rate=s_rate_eff,
-                              events='stim_ttl', date=date, inventory=inventory[r],
-                              evnts_compare=bhv_strt_stim_sec)
+                elif len(b_f) > 1:
+                    print('---')
+                    print(date+': several behavioral files found')
+                    print(b_f)
+                    sorted_files = order_by_sufix(file_list=b_f)
+                    print('Files:')
+                    print(sorted_files)
+                    print('Used file: ', sorted_files[-1])
+                    b_f = [sorted_files[-1]]
+                p.load(b_f[0])
+                p.process()
+                p.trial_sess.head()  # preprocessed df stored in attr. trial_sess
+                df = p.sess
+                bhv_strt_stim_sec, _ = utils.get_startSound_times(df=df)
+                bhv_strt_stim_sec -= bhv_strt_stim_sec[0]
+                try:
+                    samples = utils.get_electro(path=e_f, s_rate=s_rate,
+                                                s_rate_eff=s_rate_eff)
+                except IndexError:
+                    print('Electro file .dat not found in '+e_f)
+                    if len(e_f_bis):
+                        print('Trying folder '+e_f_bis[0])
+                        samples = utils.get_electro(path=e_f_bis[0],
+                                                    s_rate=s_rate,
+                                                    s_rate_eff=s_rate_eff)
+                    else:
+                        inventory[r]['no_electro'].append(date)
+                        continue
+                # get stim ttl starts/ends
+                check_stim_starts(samples=samples, chnls=[35, 36],  date=date,
+                                  s_rate=s_rate_eff, events='stim_ttl',
+                                  evnts_compare=bhv_strt_stim_sec,
+                                  inventory=inventory[r])
 
-            np.savez('/home/molano/fof/inventory.npz', **inventory)
+                np.savez('/home/molano/fof/inventory.npz', **inventory)
 
 
 if __name__ == '__main__':

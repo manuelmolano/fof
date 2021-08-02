76290638a354d33147b16adc7f6e3716808213d6
manuelmolano <manuelmolanomazon@gmail.com>
2021-08-02 14:26:04 +0200

    minor

--- inventory.py
+++ inventory.py
@@ -104,7 +104,7 @@ def inventory(s_rate=3e4, s_rate_eff=2e3):
                               events='stim_ttl', date=date, inventory=inventory[r],
                               evnts_compare=bhv_strt_stim_sec)
 
-            np.savez(spks_sort_folder+'/inventory.npz', **inventory)
+            np.savez('/inventory.npz', **inventory)
 
 
 if __name__ == '__main__':

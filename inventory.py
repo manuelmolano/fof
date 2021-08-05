aa1fcbf1d82e5dbb177fa07d2ada4ea9a58424a2
molano <you@example.com>
2021-08-05 16:09:28 +0200

    fix bug with bhv folder not found

--- inventory.py
+++ inventory.py
@@ -112,8 +112,8 @@ def inventory(s_rate=3e4, s_rate_eff=2e3, redo=False):
             print('Files:')
             print(sorted_files)
             print('Used file: ', sorted_files[-1])
-            b_f = [sorted_files[-1]]
-        return b_f[0]
+            b_f = sorted_files[-1]
+        return b_f
 
     def load_behavior():
         try:

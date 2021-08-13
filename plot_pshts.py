b515b65684d1edee12424e6eaafae1f45e27d111
manuelmolano <manuelmolanomazon@gmail.com>
2021-08-13 11:44:20 +0200

    create psth-cond-choice fn

--- plot_pshts.py
+++ plot_pshts.py
@@ -28,27 +28,34 @@ def histogram_psth(spk_times, events, bins, bin_size):
 
 
 def convolve_psth(spk_times, events, std=20, margin_psth=1000):
-    krnl_len = 5*std
-    # pass spikes to ms
-    spk_times = 1000*spk_times.flatten()
-    spk_times = spk_times.astype(int)
-    spk_dist = spk_times[-1] - spk_times[0]
-    # build spike vector
-    spk_vct = np.zeros((spk_dist+2*margin_psth, ))
-    spk_vct[spk_times-spk_times[0]+margin_psth] = 1
-    # convolve
-    x = np.linspace(norm.ppf(1e-5, scale=std),
-                    norm.ppf(1-1e-5, scale=std), krnl_len)
-    kernel = norm.pdf(x, scale=std)
-    kernel = kernel/np.sum(kernel)  # XXX: why isn't kernel already normalized?
-    spk_conv = np.convolve(spk_vct, kernel)
-    # spk_conv = gaussian_filter1d(spk_vct, std)
-    # pass events to ms
-    events = 1000*events
-    # offset events
-    events = events.astype(int)-spk_times[0]+margin_psth
-    peri_evs = np.array([spk_conv[x-margin_psth:x+margin_psth] for x in events])
-    psth = np.mean(peri_evs, axis=0)*1000
+    if len(events) > 0:
+        krnl_len = 5*std
+        # pass spikes to ms
+        spk_times = 1000*spk_times.flatten()
+        spk_times = spk_times.astype(int)
+        spk_dist = spk_times[-1] - spk_times[0]
+        # build spike vector
+        spk_vct = np.zeros((spk_dist+2*margin_psth, ))
+        spk_vct[spk_times-spk_times[0]+margin_psth] = 1
+        # convolve
+        x = np.linspace(norm.ppf(1e-5, scale=std),
+                        norm.ppf(1-1e-5, scale=std), krnl_len)
+        kernel = norm.pdf(x, scale=std)
+        kernel = kernel/np.sum(kernel)  # XXX: why isn't kernel already normalized?
+        spk_conv = np.convolve(spk_vct, kernel)
+        # spk_conv = gaussian_filter1d(spk_vct, std)
+        # pass events to ms
+        events = 1000*events
+        # offset events
+        events = events.astype(int)-spk_times[0]+margin_psth
+        peri_evs = np.array([spk_conv[x-margin_psth:x+margin_psth]
+                             for x in events])
+        try:
+            psth = np.mean(peri_evs, axis=0)*1000
+        except:
+            print(1)
+    else:
+        psth = []
     return psth
 
 
@@ -70,6 +77,38 @@ def plt_psths(spks, clstrs, sel_clstrs, clstrs_qlt, evs, ax, ncols, nrows,
         if i_cl == ncols*(nrows-1):
             ax[i_cl].set_xlabel('Peri-stimulus time (s)')
             ax[i_cl].set_ylabel('Firing rate (Hz)')
+
+
+def psth_choice_cond(e_file, b_file, session, ev='stim_ttl_strt', std_conv=20,
+                     margin_psth=1000, sv_folder=''):
+    e_data = np.load(e_file, allow_pickle=1)
+    b_data = pd.read_pickle(b_file)
+    trial_times = ut.date_2_secs(b_data.fix_onset_dt)
+    events = e_data[ev]
+    ev_indx = np.searchsorted(trial_times, events)-1
+    _, counts = np.unique(ev_indx, return_counts=1)
+    if np.max(counts) != 1:
+        print(np.unique(counts, return_counts=1))
+    choice = b_data['R_response'].values
+    sel_clstrs = e_data['sel_clstrs']
+    if len(sel_clstrs) > 0:
+        ncols = int(np.ceil(np.sqrt(len(sel_clstrs))))
+        nrows = (ncols-1) if ncols*(ncols-1) >= len(sel_clstrs) else ncols
+        f, ax = plt.subplots(ncols=ncols, nrows=nrows, figsize=(18, 8))
+        ax = ax.flatten() if nrows > 1 and ncols > 1 else ax
+        ax = [ax] if nrows == 1 and ncols == 1 else ax
+        plt_psths(spks=e_data['spks'], clstrs=e_data['clsts'],
+                  sel_clstrs=e_data['sel_clstrs'], clstrs_qlt=e_data['clstrs_qlt'],
+                  evs=events[choice[ev_indx] > .5], ax=ax, ncols=ncols,
+                  nrows=nrows, std_conv=std_conv, margin_psth=margin_psth,
+                  lbl='Right')
+        plt_psths(spks=e_data['spks'], clstrs=e_data['clsts'],
+                  sel_clstrs=e_data['sel_clstrs'], clstrs_qlt=e_data['clstrs_qlt'],
+                  evs=events[choice[ev_indx] < .5], ax=ax, ncols=ncols,
+                  nrows=nrows, std_conv=std_conv, margin_psth=margin_psth,
+                  lbl='Left')
+        f.savefig(sv_folder+session+'_'+ev+'.png')
+        plt.close(f)
 
 
 if __name__ == '__main__':
@@ -95,32 +134,34 @@ if __name__ == '__main__':
             idx = [i for i, x in enumerate(inv['session']) if x.endswith(session)]
             if len(idx) != 1:
                 print(str(idx))
-            e_file = sess+'/e_data.npz'
-            b_file = sess+'/df_trials'
-            e_data = np.load(e_file, allow_pickle=1)
-            b_data = pd.read_pickle(b_file)
-            trial_times = ut.date_2_secs(b_data.fix_onset_dt)
-            ev = 'stim_ttl_strt'
-            events = e_data[ev]
-            ev_indx = np.searchsorted(trial_times, events)-1
-            _, counts = np.unique(ev_indx, return_counts=1)
-            assert np.max(counts) == 1
-            choice = b_data['R_response'].values
-            sel_clstrs = e_data['sel_clstrs']
-            ncols = int(np.ceil(np.sqrt(len(sel_clstrs))))
-            nrows = (ncols-1) if ncols*(ncols-1) >= len(sel_clstrs) else ncols
-            f, ax = plt.subplots(ncols=ncols, nrows=nrows, figsize=(18, 8))
-            ax = ax.flatten()
-            plt_psths(spks=e_data['spks'], clstrs=e_data['clsts'],
-                      sel_clstrs=e_data['sel_clstrs'],
-                      clstrs_qlt=e_data['clstrs_qlt'],
-                      evs=events[choice[ev_indx] > .5],
-                      ax=ax, ncols=ncols, nrows=nrows,
-                      std_conv=std_conv, lbl='Right')
-            plt_psths(spks=e_data['spks'], clstrs=e_data['clsts'],
-                      sel_clstrs=e_data['sel_clstrs'],
-                      clstrs_qlt=e_data['clstrs_qlt'],
-                      evs=events[choice[ev_indx] < .5],
-                      ax=ax, ncols=ncols, nrows=nrows,
-                      std_conv=std_conv, lbl='Left')
-            f.savefig(sv_folder+session+'.png')
+                continue
+            else:
+                print(str(inv['num_stms_csv'][idx[0]])+' / ' +
+                      str(inv['sil_per'][idx[0]])+' / ' +
+                      str(inv['num_stim_ttl'][idx[0]])+' / ' +
+                      str(inv['stim_ttl_dists_med'][idx[0]])+' / ' +
+                      str(inv['stim_ttl_dists_max'][idx[0]])+' / ' +
+                      str(inv['num_stim_analogue'][idx[0]])+' / ' +
+                      str(inv['stim_analogue_dists_med'][idx[0]])+' / ' +
+                      str(inv['stim_analogue_dists_max'][idx[0]]))
+            if inv['sil_per'][idx[0]] < 1e-2:
+                if inv['stim_ttl_dists_max'][idx[0]] < 0.1:
+                    e_file = sess+'/e_data.npz'
+                    b_file = sess+'/df_trials'
+                    ev = 'stim_ttl_strt'
+                    psth_choice_cond(e_file=e_file, b_file=b_file, session=session,
+                                     std_conv=std_conv, margin_psth=margin_psth,
+                                     sv_folder=sv_folder)
+                    ev = 'fix_strt'
+                    psth_choice_cond(e_file=e_file, b_file=b_file, session=session,
+                                     std_conv=std_conv, margin_psth=margin_psth,
+                                     sv_folder=sv_folder)
+                    ev = 'outc_strt'
+                    psth_choice_cond(e_file=e_file, b_file=b_file, session=session,
+                                     std_conv=std_conv, margin_psth=margin_psth,
+                                     sv_folder=sv_folder)
+                if inv['stim_analogue_dists_max'][idx[0]] < 0.1:
+                    ev = 'stim_anlg_strt'
+                    psth_choice_cond(e_file=e_file, b_file=b_file, session=session,
+                                     std_conv=std_conv, margin_psth=margin_psth,
+                                     sv_folder=sv_folder)

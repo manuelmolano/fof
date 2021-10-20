1ec2e21284981ebb65f979cd98581bca1311a5ad
manuelmolano <manuelmolanomazon@gmail.com>
2021-10-20 18:08:31 +0200

    fix type issue

--- nGLM.py
+++ nGLM.py
@@ -790,7 +790,7 @@ if __name__ == '__main__':
         if len(sys.argv) == 1:
             lag = 0
         else:
-            lag = sys.argv[1]
+            lag = int(sys.argv[1])
         print('Using lag: ', lag)
         # main_folder = '/home/molano/Dropbox/project_Barna/FOF_project/' +\
         #     'networks/pretrained_RNNs_N2_fina_models/test_2AFC_activity/'
